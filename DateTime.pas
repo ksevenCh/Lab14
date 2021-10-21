8cca7946d1946a4562e6c10183b44339c40cb50d
Ксения Чекомасова <ksevenCh@gmail.com>
2021-10-21 17:41:14 +0300

    Add SecondsInMinute to DateTime

--- DateTime.pas
+++ DateTime.pas
@@ -3,6 +3,10 @@ begin
   if (a mod 4 = 0) and ((a mod 100 <> 0) or (a mod 400 = 0)) then Result := True else Result := false;
 end;
 
+function secondInMinute(a : integer) : integer;
+begin
+  result := a * 60;
+end;
 
 
 begin
